62e128366b0a303e1bd1a828acf9995ec179a9d2
Martin Bedoya <macowner@macs-MBP.attlocal.net>
2021-10-19 20:48:49 -0500

    added get logic to get methods and system responses with notes.html and index.html

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -1,2 +1,17 @@
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
+const html = require('express').Router();
+
+// GET Route for retrieving all the tips
+
+html.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
+
+html.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+
+
+module.exports = html;
+
