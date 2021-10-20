50b9fcca6de0eb0f272560f8677c8ac5a908be68
Martin Bedoya <macowner@macs-MBP.attlocal.net>
2021-10-19 21:14:56 -0500

    Added post method logic

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,12 +1,15 @@
-const { response } = require('express');
+
 const fs = require('fs');
 // const path = require('path');
+const uuid = require('./helpers/uuid');
 const api = require('express').Router();
 
+// Get method to read data from API
 api.get('/api/notes', (req, res)=>{
     fs.readFile('./db/db.json', (err, data)=>{
         if (err){
             console.error(err);
+            alert(err);
         } else {
             let notes = JSON.parse(data);
             res.json(notes);
@@ -14,6 +17,31 @@ api.get('/api/notes', (req, res)=>{
     });
 });
 
+// Post method here
+api.post('/api/notes', (req, res)=>{
+    const newNote = req.body;
+    newNote.id = uuid();
+
+    fs.readFile('./db/db.json', (err, data)=>{
+        if (err){
+            console.error(err);
+            alert(err);
+        } else {
+            const notes = JSON.parse(data);
+            notes.push(newNote)
+        }
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (err)=>{
+        if (err){
+            console.error(err);
+            alert(err);
+        } else {
+            res.json(newNote);
+        }
+    })
+    
+
+    })
+})
 
 
 module.exports = api;
