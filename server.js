e12744899fee9c5510fb9f4174cd75d92e926a9d
Martin Bedoya <macowner@macs-MBP.attlocal.net>
2021-10-19 20:40:53 -0500

    added const api and const html to require both routes, also added middleware

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const PORT = process.env.PORT || 3001;
-const fs = require('fs');
 const path = require('path');
-
 const express = require('express');
 const app = express();
 
-const notesList = require('./db/db.json');
+// requiring routes for api and html routes
+const api = require('./routes/apiRoute');
+const html = require('./routes/htmlRoute');
+
+// requiring db.json
+// const notesList = require('./db/db.json');
+
+app.use('/api', api);
+app.use('/html',html);
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
+
+// console log listening to port
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
