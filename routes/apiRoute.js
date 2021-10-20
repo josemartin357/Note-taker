82dc93650d21930aa0ee26cf6bf576e8f2e01acd
Martin Bedoya <macowner@macs-MBP.attlocal.net>
2021-10-19 21:03:23 -0500

    added GET method, requires api const from router

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,2 +1,19 @@
+const { response } = require('express');
 const fs = require('fs');
-const path = require('path');
+// const path = require('path');
+const api = require('express').Router();
+
+api.get('/api/notes', (req, res)=>{
+    fs.readFile('./db/db.json', (err, data)=>{
+        if (err){
+            console.error(err);
+        } else {
+            let notes = JSON.parse(data);
+            res.json(notes);
+        }
+    });
+});
+
+
+
+module.exports = api;
