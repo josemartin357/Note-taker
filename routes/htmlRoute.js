27dde99e60443381c28e7a62b9875f7c6040f787
Martin Bedoya <macowner@macs-MBP.attlocal.net>
2021-10-19 21:59:32 -0500

    changed source in get method to /notes.. still having issue pulling notes.html

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -2,12 +2,12 @@
 const path = require('path');
 const html = require('express').Router();
 
-html.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/assets/notes.html'));
+html.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
     });
 
 html.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/assets/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
     });
 
 
